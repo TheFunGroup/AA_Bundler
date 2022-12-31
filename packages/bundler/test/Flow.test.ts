c892c020c03b151c436e703f98e7f6c7347351b6
aaronchewbani <aaronchewbani@gmail.com>
2022-12-30 17:54:33 -0800

    bundler user op call

--- packages/bundler/test/Flow.test.ts
+++ packages/bundler/test/Flow.test.ts
@@ -1,21 +1,25 @@
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import hre, { ethers } from 'hardhat'
-import sinon from 'sinon'
+import { parseEther } from 'ethers/lib/utils'
 
+import sinon from 'sinon'
+import simpleAccountABI from '../src/SimpleAccountABI'
 import * as SampleRecipientArtifact
   from '@account-abstraction/utils/artifacts/contracts/test/SampleRecipient.sol/SampleRecipient.json'
 
 import { BundlerConfig } from '../src/BundlerConfig'
-import { ERC4337EthersProvider, ERC4337EthersSigner, ClientConfig, wrapProvider } from '@account-abstraction/sdk'
-import { Signer, Wallet } from 'ethers'
+import { ERC4337EthersProvider, ERC4337EthersSigner, ClientConfig, wrapProvider, HttpRpcClient, SimpleAccountAPI, DeterministicDeployer } from '@account-abstraction/sdk'
+import { Contract, Signer, Wallet } from 'ethers'
 import { runBundler } from '../src/runBundler'
 import { BundlerServer } from '../src/BundlerServer'
 import fs from 'fs'
-
+import { SimpleAccountDeployer__factory } from '@account-abstraction/contracts'
+const Web3 = require('web3')
+let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 const { expect } = chai.use(chaiAsPromised)
 
-export async function startBundler (options: BundlerConfig): Promise<BundlerServer> {
+export async function startBundler(options: BundlerConfig): Promise<BundlerServer> {
   const args: any[] = []
   args.push('--beneficiary', options.beneficiary)
   args.push('--entryPoint', options.entryPoint)
@@ -64,7 +68,7 @@ describe('Flow', function () {
       minBalance: '0',
       mnemonic: mnemonicFile,
       network: 'http://localhost:8545/',
-      port: '5555'
+      port: '3000'
     })
   })
 
@@ -78,7 +82,7 @@ describe('Flow', function () {
   it('should send transaction and make profit', async function () {
     const config: ClientConfig = {
       entryPointAddress,
-      bundlerUrl: 'http://localhost:5555/rpc'
+      bundlerUrl: 'http://localhost:3000/rpc'
     }
 
     // use this as signer (instead of node's first account)
@@ -99,12 +103,101 @@ describe('Flow', function () {
 
     const sampleRecipientContract =
       new ethers.Contract(sampleRecipientAddress, SampleRecipientArtifact.abi, erc4337Signer)
-    console.log(sampleRecipientContract.address)
+    // console.log(sampleRecipientContract.address)
 
     const result = await sampleRecipientContract.something('hello world')
-    console.log(result)
+    // console.log(result)
     const receipt = await result.wait()
-    console.log(receipt)
+    // console.log(receipt)
+  })
+
+  it('should send transaction and make profit', async function () {
+    const config: ClientConfig = {
+      entryPointAddress,
+      bundlerUrl: 'http://localhost:3000/rpc'
+    }
+
+    // use this as signer (instead of node's first account)
+    const ownerAccount = Wallet.createRandom()
+    erc4337Provider = await wrapProvider(
+      ethers.provider,
+      // new JsonRpcProvider('http://localhost:8545/'),
+      config,
+      ownerAccount
+    )
+    // console.log(erc4337Provider)
+    // erc4337Signer = erc4337Provider.getSigner()
+    // const simpleAccountPhantomAddress = await erc4337Signer.getAddress()
+
+    let addr = "0xB1d3BD3E33ec9A3A15C364C441D023a73f1729F6"
+    let amount = parseEther('.01') //amount
+    let accountOwner = new Wallet('0x'.padEnd(66, '7'))
+    const dep = new DeterministicDeployer(this.provider)
+    const accountDeployer = await dep.getDeterministicDeployAddress(SimpleAccountDeployer__factory.bytecode)
+
+
+    const net = await erc4337Provider.getNetwork()
+
+
+    const rpcClient = new HttpRpcClient(config.bundlerUrl, config.entryPointAddress, net.chainId)
+
+    const provider = ethers.provider
+
+    const accountApi = new SimpleAccountAPI({
+      provider: erc4337Provider,
+      entryPointAddress: config.entryPointAddress,  //check this
+      owner: accountOwner,
+      factoryAddress: accountDeployer
+    })
+
+    // let contract = new web3.eth.Contract(simpleAccountABI);  //get from simpleaccount
+    // let data = contract.methods.transfer(addr, amount).encodeABI();
+
+    //   function transfer(address payable dest, uint256 amount) external onlyOwner {
+    //     dest.transfer(amount);
+    // }
+    let data = web3.eth.abi.encodeFunctionCall({
+      name: 'transfer',
+      type: 'function',
+      inputs: [
+        {
+          type: 'address',
+          name: 'dest'
+        }, {
+
+          type: 'uint256',
+          name: 'amount'
+        }]
+    }, [addr, amount]);
+
+    const userOp = await accountApi.createSignedUserOp({
+      target:addr,
+      data
+    })
+
+    try {
+      const userOpHash = await rpcClient.sendUserOpToBundler(userOp)
+      const txid = await accountApi.getUserOpReceipt(userOpHash)
+      console.log('reqId', userOpHash, 'txid=', txid)
+    } catch (e: any) {
+      console.log(e)
+    }
+    //bundler calls transfer
+
+
+    // await signer.sendTransaction({
+    //   to: simpleAccountPhantomAddress,
+    //   value: 10e18.toString()
+    // })
+
+    // const sampleRecipientContract =
+    //   new ethers.Contract(sampleRecipientAddress, SampleRecipientArtifact.abi, erc4337Signer)
+    // console.log(sampleRecipientContract.address)
+
+    // const result = await sampleRecipientContract.something('hello world')
+    // console.log(result)
+    // const receipt = await result.wait()
+    // console.log(receipt)
   })
 
   it.skip('should refuse transaction that does not make profit', async function () {
